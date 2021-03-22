e9f8082a2b20d40af2cfc667efdcd40b4eff12ce
Dayana DM <s202675@student.dtu.dk>
2021-03-22 09:53:44 +0000

    commit of first function

---
+++ R/random_dna.R
@@ -0,0 +1,9 @@
+# R/random_dna.R
+#generates a random DNA sequence of length l
+#' @export
+
+random_dna <- function(l){
+  nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
+  dna = paste0(nucleotides, collapse = "")
+  return(dna)
+}
